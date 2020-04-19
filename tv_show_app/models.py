19c10eb8df3d12ae63a32763fd5340ef47fc6f89
Daniel <ddelachica89@gmail.com>
2020-04-19 15:11:00 -0500

    added validations

--- tv_show_app/models.py
+++ tv_show_app/models.py
@@ -1,4 +1,18 @@
+from __future__ import unicode_literals
 from django.db import models
+
+
+class ShowManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData["title"]) < 2:
+            errors["name"] = "Title name should be at least 2 characters"
+        if len(postData["network"]) < 3:
+            errors["network"] = "Network should be at least 10 characters"
+        if len(postData["desc"]) < 10:
+            errors["desc"] = "Description should be at least 10 characters"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=50)
@@ -7,3 +21,5 @@ class Show(models.Model):
     desc = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
+
--- tv_show_app/views.py
+++ tv_show_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .models import Show
+from django.contrib import messages
 
 def index(request):
     return redirect('/shows')
@@ -12,24 +13,24 @@ def shows(request):
     
     return render(request, "index.html", context)
 
-###########################
-# DISPLAY ADD SHOW TEMPLATE
-###########################
+
 def new_show(request):
     
     return render(request, "new.html")
 
-###########################
-# ADD SHOW TO DB
-###########################
+
 def add_new_show(request):
-    new_show = Show.objects.create(title=request.POST["title"], network=request.POST["network"], release=request.POST["release"], desc = request.POST["desc"])
-    id = new_show.id
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect("/shows/new")
+    else:
+        new_show = Show.objects.create(title=request.POST["title"], network=request.POST["network"], release=request.POST["release"], desc = request.POST["desc"])
+        id = new_show.id
     return redirect(f"/shows/{id}")
 
-###########################
-# DISPLAY SHOW INFO
-###########################
+
 def display_show(request, id):
     
     new_show = Show.objects.get(id=id)
@@ -40,9 +41,7 @@ def display_show(request, id):
     return render(request, "show.html", context)
 
 
-###########################
-# EDIT SHOW ENTRY
-###########################
+
 def edit_show(request, id):
     show = Show.objects.get(id=id)
     context= {
@@ -50,54 +49,26 @@ def edit_show(request, id):
     }
     return render(request, "edit.html", context)
 
-###########################
-# SUBMIT EDIT
-###########################
-def update(request, id):
-    show = Show.objects.get(id=id)
-    show.title = request.POST['title']
-    show.network = request.POST['network']
-    show.release = request.POST['release']
-    show.desc = request.POST['desc']
-    show.save()
-    return redirect(f"/shows/{id}")
 
-###########################
-# REMOVE SHOW FROM DB
-###########################
+def update(request, id):
+    errors = Show.objects.basic_validator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f"/shows/{id}/edit")
+    else:
+        show = Show.objects.get(id=id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release = request.POST['release']
+        show.desc = request.POST['desc']
+        show.save()
+        return redirect(f"/shows/{id}")
+
+
 def delete(request, id):
     
     show = Show.objects.get(id=id)
     show.delete()
     
     return redirect("/shows")
-# def new(request):
-#     return render(request, 'new.html')
-
-# def edit(request, id):
-#     context = {
-#         'show': Show.objects.get(id=id)
-#     }
-#     return render(request, 'edit.html',context)
-
-
-# def shows(request, id):
-#     if id > 0 :
-#         context = {
-#             'show': Show.objects.get(id=id)
-#         }
-#         return render(request, 'show.html',context)
-#     else:    
-#         context = {
-#         'show': Show.objects.all()
-#         }
-#         return render(request, 'index.html', context)
-
-# def create(request):
-#     new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release=request.POST['release'], desc=request.POST['desc'])
-#     id = new_show.id
-#     print(new_show)
-#     return redirect (f'/shows/{id}')
-
-# def update(request):
-#     return redirect ('shows/')
